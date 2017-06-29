67b8b22c1aaa2facd5dbc8b79dbbf7cc79d9231b
Toshiyuki Takahashi <toshi@metahack.jp>
2017-06-29 13:17:11 +0900

    bugfix: gettimeofday and settimeofday

--- test_mck/src/gettimeofday/000.c
+++ test_mck/src/gettimeofday/000.c
@@ -10,19 +10,24 @@ SETUP_EMPTY(TEST_SUITE, TEST_NUMBER)
 
 RUN_FUNC(TEST_SUITE, TEST_NUMBER)
 {
-	struct timeval set, get;
+	struct timeval set, get, before;
+
+	/* get time before testing */
+	errno = 0;
+	gettimeofday(&before, NULL);
+	tp_assert(errno == 0, "gettimeofday error. 1");
 
 	/* set the time */
 	set.tv_sec = 6;
 	set.tv_usec = 6;
 	errno = 0;
 	settimeofday(&set, NULL);
-	tp_assert(errno == 0, "settimeofday error.");
+	tp_assert(errno == 0, "settimeofday error. 1");
 
 	/* get the time */
 	errno = 0;
 	gettimeofday(&get, NULL);
-	tp_assert(errno == 0, "gettimeofday error.");
+	tp_assert(errno == 0, "gettimeofday error. 2");
 	tp_assert(get.tv_sec <= (set.tv_sec + 1), "gotten timeval is too high.");
 	tp_assert((get.tv_sec > set.tv_sec) ||
 		  (get.tv_sec == set.tv_sec && get.tv_usec > set.tv_usec),
@@ -30,6 +35,17 @@ RUN_FUNC(TEST_SUITE, TEST_NUMBER)
 
 	printf("set = {tv_sec = %ld, tv_usec = %ld}\n", set.tv_sec, set.tv_usec);
 	printf("get = {tv_sec = %ld. tv_usec = %ld}\n", get.tv_sec, get.tv_usec);
+
+	/* restore timer */
+	before.tv_sec += get.tv_sec - set.tv_sec;
+	before.tv_usec += get.tv_usec - set.tv_usec;
+	if (before.tv_usec >= 1000000) {
+		before.tv_sec += before.tv_usec / 1000000;
+		before.tv_usec += before.tv_usec % 1000000;
+	}
+	errno = 0;
+	settimeofday(&before, NULL);
+	tp_assert(errno == 0, "settimeofday error. 2");
 
 	/* test succeeeds */
 	return NULL;
