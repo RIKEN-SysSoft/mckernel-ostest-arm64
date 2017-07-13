92f55ab97b05db585e04c08ffe98026488b0451a
Toshiyuki Takahashi <toshi@metahack.jp>
2017-07-13 16:07:00 +0900

    Fix lv07-pth

--- lv07_read-write_with-glibc/entry-pth.c
+++ lv07_read-write_with-glibc/entry-pth.c
@@ -1,18 +1,24 @@
 /* entry-pth.c COPYRIGHT FUJITSU LIMITED 2015-2017 */
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 
 #define MAX_THREAD_NUM 64
 
+int thread_num;
 char *filepath;
+pthread_barrier_t barrier;
 
 void read_write(int num, const char *path) {
 	FILE* fp;
 	char buf[256];
 	size_t cnt;
+	int i;
+
 	if ((fp=fopen(path,"r")) == NULL) {
-		perror("fopen faild: ");
+		perror("fopen failed: ");
 		return;
 	}
 
@@ -34,7 +40,16 @@ void read_write(int num, const char *path) {
 	cnt = fread(buf+2, sizeof(buf[0]), sizeof(buf)/sizeof(buf[0])-2, fp);
 	buf[cnt+2] = '\0';
 
-	printf("%s\n", buf);
+	for (i = 0; i < num; i++) {
+		pthread_barrier_wait(&barrier);
+	}
+
+	printf("%d: %s\n", num, buf);
+
+	for (; i < thread_num; i++) {
+		pthread_barrier_wait(&barrier);
+	}
+
 	fclose(fp);
 }
 
@@ -47,39 +62,55 @@ void *threadFunction( void* argument ) {
 
 int main(int argc, char** argv) {
 	pthread_t child[MAX_THREAD_NUM];
-	int thread_num = 2;	/* default thread_num 2 */
 	int i = 0, ret = 0;
 	int tn[MAX_THREAD_NUM];
+	int ncpu;
+	struct rlimit rlimit_nproc;
 
-	if (argc == 1 || 3 < argc) {
-		printf("invalid argment. %s ostype-filepath <thread_num[default=2]>\n", argv[0]);
+	if (argc != 4) {
+		printf("invalid argment. %s ostype-filepath <thread_num> <rlimit_nproc>\n", argv[0]);
 		return 0;
 	}
 
 	filepath = argv[1];
 
-	if (argc != 2) {
-		thread_num = atoi(argv[2]);
+	thread_num = atoi(argv[2]);
 
-		if ((thread_num < 1) || (MAX_THREAD_NUM < thread_num)) {
-			printf("thread_number invalid, 1 <= thread_num <= %d\n", MAX_THREAD_NUM);
-			return 0;
-		}
-		printf("use thread_num = %d.\n", thread_num);
+	if ((thread_num < 1) || (MAX_THREAD_NUM < thread_num)) {
+		printf("thread_number invalid, 1 <= thread_num <= %d\n", MAX_THREAD_NUM);
+		return 0;
+	}
+	printf("use thread_num = %d.\n", thread_num);
 
-	} else {
-		printf("use default thread_num = 2.\n");
+	ncpu = atoi(argv[3]);
+	getrlimit(RLIMIT_NPROC, &rlimit_nproc);
+	if (ncpu <= 0 || ncpu > rlimit_nproc.rlim_max) {
+		printf("rlimit_nproc invalid, 1 <= rlimit_nproc <= rlim_max\n");
+		return 0;
+	}
+	rlimit_nproc.rlim_cur = ncpu;
+	ret = setrlimit(RLIMIT_NPROC, &rlimit_nproc);
+	if (ret != 0) {
+		printf("setrlimit error\n");
+		return 0;
+	}
+	
+	for (i = 0; i < thread_num; i++) {
+		tn[i] = i;
 	}
 
-	for (i = 0; i < MAX_THREAD_NUM; i++) {
-		tn[i] = i;
+	ret = pthread_barrier_init(&barrier, NULL, thread_num);
+	if (ret != 0) {
+		printf("pthread_barrier_init error\n");
+		return 0;
 	}
 
 	for(i = 1; i < thread_num; i++) {
 		// Call the clone system call to create the child thread 
 		ret = pthread_create(&child[i], NULL, threadFunction, (void *)&tn[i]);
 		if(ret) {
-			printf("Faild to create thread[%d]. ret = %d\n", i, ret);
+			printf("Failed to create thread[%d]. ret = %d\n", i, ret);
+			return 0;
 		}
 	}
 
@@ -88,7 +119,8 @@ int main(int argc, char** argv) {
 	for(i = 1; i < thread_num; i++) {
 		ret = pthread_join(child[i], NULL);
 		if(ret) {
-			printf("Faild to join thread[%d]. ret = %d\n", i, ret);
+			printf("Failed to join thread[%d]. ret = %d\n", i, ret);
+			return 0;
 		}
 	}
 
