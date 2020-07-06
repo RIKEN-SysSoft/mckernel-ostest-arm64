1822d413dd3bcab30e76bd03432b4959f393c8cd
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-07-06 14:21:52 +0900

    freeze_thaw: add sudo

--- util/exec/freeze_thaw.000.sh
+++ util/exec/freeze_thaw.000.sh
@@ -1,12 +1,12 @@
 . ${AUTOTEST_HOME}/ostest/util/exec/background.sh
 sleep 1
 
-"${app_dir}/freeze_thaw" 0 freeze
+sudo "${app_dir}/freeze_thaw" 0 freeze
 for i in `seq 1 3`
 do
     sleep 1
     echo "sleep ${i} second elapsed."
 done
-"${app_dir}/freeze_thaw" 0 thaw
+sudo "${app_dir}/freeze_thaw" 0 thaw
 
 . ${AUTOTEST_HOME}/ostest/util/exec/wait.sh
